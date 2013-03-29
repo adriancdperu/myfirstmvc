202fc17d07633b91afefe3e4b546a068e7db197a
Adrian <adrian@investometrica.com>
2013-03-29 17:50:29 +0900

    Create DomainManager.pm

---
+++ DomainManager.pm
@@ -0,0 +1,20 @@
+######################## Model ########################
+
+
+# Business logic and validation comes here.
+# Motivation; configuring OS for new virtual host.
+
+
+package DomainManager;
+
+sub install {
+    my ($this, $domain) = @_;
+    # instructions
+    return my @results;
+}
+
+sub uninstall {
+
+}
+
+1;
