9f1ba2c9c2309af311a50b6b98eda71df9b294f3
Adrian <adrian@investometrica.com>
2013-03-29 17:51:14 +0900

    Create Domain.pm

---
+++ Domain.pm
@@ -0,0 +1,16 @@
+######################## Model ########################
+
+
+# Business logic and validation comes here.
+# Motivation; configuring OS for new virtual host.
+
+
+package Domain;
+
+sub load {...}
+sub getAddress {...}
+
+#
+
+1;
+
